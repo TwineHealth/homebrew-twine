812734cfc1a22f659ad58293b1328ca0b32e41c1
Deena Wang <deenawang@twinehealth.com>
2018-01-12 15:47:05 -0500

    Adds require

--- Formula/mongodb@3.2.18.rb
+++ Formula/mongodb@3.2.18.rb
@@ -1,6 +1,4 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook.html
-#                http://www.rubydoc.info/github/Homebrew/brew/master/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+require "language/go"
 
 class MongodbAT3218 < Formula
   desc "High-performance, schema-free, document-oriented database"
